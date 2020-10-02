5cf3eef572b1978cbeb5160f962783bbc2d8de26
maisevich <volha.maisevich@softclub.by>
2020-10-02 18:05:28 +0300

    feat: react-reux-app

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,9 @@
+import {combineReducers} from "redux";
+import {postReducer} from "./postsReducer";
+import {appReducer} from "./appReducer";
+
+
+export const rootReducer = combineReducers({
+        posts: postReducer,
+        app: appReducer
+});
